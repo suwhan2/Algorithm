7eecaf41ee6279416338f2b5d6580179eaafd4b6
ChoiSuHwan <jerry5230@naver.com>
2024-03-09 10:33:24 +0900

    [Silver II] Title: N과 M (9), Time: 208 ms, Memory: 21488 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int selected[], arr[],visited[];
+
+    static StringBuilder sb = new StringBuilder();
+    private static void pick(int cnt) {
+        if(cnt==M) {
+            for(int x : selected) {
+                sb.append(x).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int check=-1;
+        for(int i=0;i<N;i++) {
+            if(visited[i]==1||arr[i]==check) continue;
+            selected[cnt]=arr[i];
+            visited[i]=1;
+            check=arr[i];
+            pick(cnt+1);
+            visited[i]=0;
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M];
+        visited = new int[N];
+        arr = new int[N];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            arr[i]=Integer.parseInt(st2.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        pick(0);
+        System.out.println(sb);
+
+    }
+}
