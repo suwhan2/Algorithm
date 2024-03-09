890fa4846bae8aee1d973d730e758b9bcadf46ba
ChoiSuHwan <jerry5230@naver.com>
2024-03-09 10:21:06 +0900

    [Silver I] Title: 미로 탐색, Time: 140 ms, Memory: 14776 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Pair{
+	int x,y;
+	public Pair(int x, int y) {
+		this.x =x;
+		this.y=y;
+	}
+}
+
+public class Main {
+	static int n;
+	static int m;
+	
+	public static int visited[][];
+	public static Queue<Pair> q = new LinkedList<>();
+	public static int grid[][];
+	public static int distance[][];
+
+	
+	public static boolean inRange(int x, int y) {
+		return 0<=x &&x<n && 0<=y && y<m;
+	}
+	
+	public static void push(int x,int y,int num){
+
+		distance[x][y]=num;
+		visited[x][y]=1;
+		q.add(new Pair(x,y));
+	}
+	
+	public static boolean canGo(int x,int y) {
+		if(!inRange(x, y)) return false;
+		if(visited[x][y]==1 || grid[x][y]==0) return false;
+		return true;
+	}
+	
+	public static void BFS() {
+		int []dx = new int[] {1,0,-1,0};
+		int []dy = new int[] {0,1,0,-1};
+		
+		while(!q.isEmpty()) {
+			Pair curr = q.poll();
+			int x = curr.x;
+			int y= curr.y;
+			
+			for(int i=0;i<4;i++) {
+				int newX = x+dx[i];
+				int newY = y+dy[i];
+				if(canGo(newX,newY)) {
+					push(newX,newY,distance[x][y]+1);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		grid = new int[n][m];
+		visited = new int[n][m];
+		distance = new int[n][m];
+		
+		for(int i=0;i<n;i++) {
+			String s = br.readLine();
+			for(int j=0;j<m;j++) {
+				grid[i][j] = (s.charAt(j)-'0');
+			}
+		}
+		
+		push(0,0,1);
+		BFS();
+		
+		System.out.println(distance[n-1][m-1]);
+		
+		
+		
+	}
+}
