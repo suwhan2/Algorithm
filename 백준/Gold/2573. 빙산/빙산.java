cd83ae307c2edf191d0eaa1ae09fb181c973b0b0
ChoiSuHwan <jerry5230@naver.com>
2024-03-14 18:42:41 +0900

    [Gold IV] Title: 빙산, Time: 488 ms, Memory: 164052 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -6,51 +6,38 @@ import java.util.StringTokenizer;
 
 class Pair{
 	int x,y;
-
 	public Pair(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-	
 }
 class Main{
-	static int N,M;
-	static int findCount;
+	static int N,M,findCount;
 	static int grid[][],tmp[][];
-	static int startX=0;
-	static int startY=0;
-	static int ans=0;
+	static int startX,startY,ans,iceCount=0;
 	static boolean visited[][];
 	static int dx[] = new int[] {1,0,-1,0};
 	static int dy[] = new int[] {0,1,0,-1};
-	static int iceCount=0;
 	static Queue<Pair> q = new ArrayDeque<>();
 	
-	
 	private static void bfs() {
-		
 		while(!q.isEmpty()) {
 			Pair t = q.poll();
 			int x= t.x;
 			int y= t.y;
-			
 			for(int i=0;i<4;i++) {
 				int newX = x +dx[i];
 				int newY = y +dy[i];
-				
 				if(grid[newX][newY]==0) {
 					tmp[x][y]++;
 					continue;
 				}
-				
 				if(!visited[newX][newY]){
 					visited[newX][newY]=true;
 					findCount++;
-					q.offer(new Pair(newX,newY));
-					
+					q.offer(new Pair(newX,newY));	
 				}
-				
 			}
 		}
 	}
@@ -65,29 +52,20 @@ class Main{
 						grid[i][j]=0;
 						iceCount--;
 					}
-					if(a>0){
-						grid[i][j]=a;
-					
-				}
-}
-				
-			}
+					if(a>0)grid[i][j]=a;	
+            }
 		}
-		
+	}
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
 				if(grid[i][j]!=0) {
 					startX =i;
 					startY=j;
-				}
-				
+				}			
 			}
 		}
-		
 		q.offer(new Pair(startX,startY));	
-		
 	}
-	
 	
 	public static void main(String args[]) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -95,7 +73,6 @@ class Main{
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		grid = new int[N][M];
-		
 		
 		boolean check=false;
 		for(int i=0;i<N;i++) {
@@ -120,20 +97,11 @@ class Main{
 			visited[startX][startY]=true;
 			findCount=1;
 			bfs();
-
-			if(findCount!=iceCount) {
-				break;
-			}
-			//지우기
+			if(findCount!=iceCount) break;
 			erase();
-
 			ans++;
-			
 		}
-		
 		if(iceCount==0) ans=0;
 		System.out.println(ans);
-
-
 	}
 }
