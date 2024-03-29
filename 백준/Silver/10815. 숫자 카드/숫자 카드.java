e9afd6769d502411cc23676f0d5600b727a89c81
ChoiSuHwan <jerry5230@naver.com>
2024-03-29 21:30:51 +0900

    [Silver V] Title: 숫자 카드, Time: 4016 ms, Memory: 158812 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static int arr[],list[];
+	
+	private static int binarySearch(int key) {
+		int low = 0;
+		int high=N-1;
+		while(low<=high) {
+			int mid=(low+high)/2;
+			if(key==arr[mid]) return 1;
+			else if(key<arr[mid]) high=mid-1;
+			else low=mid+1;
+		}
+		return 0;
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr= new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {arr[i] = Integer.parseInt(st.nextToken());}
+		Arrays.sort(arr);		
+		M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<M;i++) {
+			System.out.print(binarySearch(Integer.parseInt(st.nextToken()))+" ");
+		}
+		
+	}
+}
