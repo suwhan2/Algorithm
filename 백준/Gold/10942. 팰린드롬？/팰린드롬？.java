78b3a06792f9f11ef1d01dc415cad545e0c03de2
ChoiSuHwan <jerry5230@naver.com>
2024-06-20 23:59:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1068 ms, Memory: 231428 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+    static int N,M,S,E;
+    static int[] numbers;
+    static int[][] memo;
+    static StringBuilder sb;
+    private static void printIsPalindrome(){
+        if(N==1){
+            sb.append(1).append(System.lineSeparator());
+            return;
+        }
+        if(memo[S][E]!=-1){
+            sb.append(memo[S][E]).append(System.lineSeparator());
+            return;
+        }
+        if(S==E){
+            memo[S][E]=1;
+            sb.append(1).append(System.lineSeparator());;
+            return;
+        }
+
+        int tmp = S+E;
+        int start=0;
+        int end=0;
+        if(tmp%2==0){
+            start=tmp/2-1;
+            end=tmp/2+1;
+        }
+        if(tmp%2==1){
+            start=tmp/2;
+            end=tmp/2+1;
+        }
+        while(S<=start){
+            if(numbers[start]!=numbers[end]){
+                memo[S][E]=0;
+                sb.append(0).append(System.lineSeparator());;
+                return;
+            }
+            memo[start][end]=1;
+            start--;
+            end++;
+        }
+        sb.append(1).append(System.lineSeparator());;
+    }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        sb=new StringBuilder();
+        numbers = new int[N+1];
+        memo = new int[N+1][N+1];
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<N+1;j++){
+                memo[i][j]=-1;
+            }
+        }
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        for(int i=1;i<N+1;i++){
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        M = Integer.parseInt(br.readLine());
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            S = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            printIsPalindrome();
+        }
+        System.out.println(sb);
+    }
+}
