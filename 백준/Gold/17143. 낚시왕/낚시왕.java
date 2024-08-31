92e162b30e1dfa03cd640ae4d707cc481f5accf3
ChoiSuHwan <jerry5230@naver.com>
2024-08-31 15:35:40 +0900

    [Gold I] Title: 낚시왕, Time: 296 ms, Memory: 26280 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -4,7 +4,6 @@ import java.io.*;
 class Shark{
     int r,c,s,d,z;
     boolean isDead;
-
     public Shark(int r, int c, int s, int d, int z, boolean isDead) {
         this.r = r;
         this.c = c;
@@ -15,32 +14,26 @@ class Shark{
     }
 }
 public class Main{
-    static int[][] sea;
-    static int[][] visited;
+    static int[][] sea,visited;
     static int riskShark=-1;
     static Shark[] sharkList;
     static int R,C,M;
     static int totalCatchSize=0;
 
     private static void checkRiskShark(int i){
-        if(riskShark==-1 || sharkList[riskShark].r>=sharkList[i].r){
-            riskShark= i;
-        }
+        if(riskShark==-1 || sharkList[riskShark].r>=sharkList[i].r) riskShark= i;
     }
     private static void move(int sharkIndex){
-
         Shark shark = sharkList[sharkIndex];
-
         //전처리
         int makeEasyR=((R-1)*2);
         int makeEasyC=((C-1)*2);
         int dir,dis,tmp;
-//        System.out.println(shark.d+"방향");
+
         switch (shark.d){
             case 1 :
                 if(shark.s>makeEasyR) shark.s = shark.s%makeEasyR;
                 tmp = shark.s;
-//                System.out.println("케이스 1 시작");
                 if(shark.s<=shark.r){
                     //그냥 계산하고 끝
                     shark.r-=shark.s;
@@ -53,7 +46,6 @@ public class Main{
                     tmp-=shark.r;
                     shark.r=0;
                 }
-
                 //계산
                 dir = tmp/(R-1);
                 dis = tmp%(R-1);
@@ -70,12 +62,10 @@ public class Main{
             case 2:
                 if(shark.s>makeEasyR) shark.s = shark.s%makeEasyR;
                 tmp = shark.s;
-//                System.out.println("케이스 2 시작");
                 if(shark.s<=(R-shark.r-1)){
                     //그냥 계산하고 끝
                     shark.r+=shark.s;
                     sharkList[sharkIndex] = shark;
-//                    System.out.println("바로탈출");
                     break;
                 }
 
@@ -97,12 +87,10 @@ public class Main{
                 }
                 if(shark.r==0) shark.d=1;
                 sharkList[sharkIndex] = shark;
-//                System.out.println("움직인 직후" +shark.r+" "+shark.c);
                 break;
             case 3:
                 if(shark.s>makeEasyC) shark.s = shark.s%makeEasyC;
                 tmp = shark.s;
-//                System.out.println("케이스 3 시작");
                 if(shark.s<=(C-shark.c-1)){
                     //그냥 계산하고 끝
                     shark.c+=shark.s;
@@ -132,12 +120,9 @@ public class Main{
             case 4:
                 if(shark.s>makeEasyC) shark.s = shark.s%makeEasyC;
                 tmp = shark.s;
-//                System.out.println("케이스 4 시작"+shark.s);
                 if(shark.s<=shark.c){
-                    //그냥 계산하고 끝
                     shark.c-=shark.s;
                     sharkList[sharkIndex] = shark;
-//                    System.out.println("걍 끝남");
                     break;
                 }
 
@@ -146,11 +131,9 @@ public class Main{
                     tmp-=shark.c;
                     shark.c=0;
                 }
-
-                //계산
                 dir = tmp/(C-1);
                 dis = tmp%(C-1);
-//                System.out.println(dir);
+
                 if(dir==0){
                     shark.d=3;
                     shark.c=dis;
@@ -168,15 +151,11 @@ public class Main{
         int tmpC = sharkList[sharkIndex].c;
         if(visited[tmpR][tmpC]==-1) visited[tmpR][tmpC]=sharkIndex;
         if(visited[tmpR][tmpC]!=-1){
-            //기존이 더큼
             if(sharkList[visited[tmpR][tmpC]].z>sharkList[sharkIndex].z){
-//                System.out.println("기존 상어가 이겼다.");
                 sharkList[sharkIndex].isDead=true;
             }
 
-            //새로운게 더 큼
             if(sharkList[visited[tmpR][tmpC]].z<sharkList[sharkIndex].z){
-//                System.out.println("새로운 상어가 이겼다");
                 sharkList[visited[tmpR][tmpC]].isDead=true;
                 visited[tmpR][tmpC]=sharkIndex;
 
@@ -206,17 +185,12 @@ public class Main{
 
         }
 
-//        move(0);
-//        System.out.println(sharkList[0].r+" "+sharkList[0].c+" "+sharkList[0].d+" "+sharkList[0].s);
-
         for(int i=0;i<C;i++){
             //상어잡기
             if(riskShark!=-1){
-//                System.out.println("잡았다"+riskShark);
                 sharkList[riskShark].isDead=true;
                 totalCatchSize+=sharkList[riskShark].z;
             }
-
             //상어 이동
             riskShark=-1;
             visited = new int[R][C];
@@ -232,14 +206,8 @@ public class Main{
                 // 위험상어 설정
                 if(!sharkList[j].isDead && sharkList[j].c==(i+1)){
                     checkRiskShark(j);
-//                    System.out.println("위험상어 = "+riskShark);
                 }
-//                System.out.println(sharkList[j].r+" "+sharkList[j].c+" "+ sharkList[j].d+" "+j);
             }
-//            System.out.println(i);
-
-//            System.out.println(totalCatchSize);
-//            System.out.println("-------------------------------");
         }
         System.out.println(totalCatchSize);
     }
