25475cd221834e7cc3c0b08f50f19eed697d21ca
ChoiSuHwan <jerry5230@naver.com>
2024-10-26 23:52:42 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 17964 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
     static int X;
@@ -14,18 +13,11 @@ public class Main{
         if(X==2) System.out.println(1);
         if(X>=3){
             memo[1]=0;
-            memo[2]=1;
-            memo[3]=1;
+            memo[2]=memo[3]=1;
             for(int i=4;i<=X;i++){
                 int tmp=memo[i-1]+1;
-                if(i%3==0){
-                    int tmp2 = memo[i/3]+1;
-                    tmp = Math.min(tmp2,tmp);
-                }
-                if(i%2==0){
-                    int tmp3 = memo[i/2]+1;
-                    tmp = Math.min(tmp3,tmp);
-                }
+                if(i%3==0) tmp = Math.min(memo[i/3]+1,tmp);
+                if(i%2==0)tmp = Math.min(memo[i/2]+1,tmp);
                 memo[i]=tmp;
             }
             System.out.println(memo[X]);
