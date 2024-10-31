a49a00ca3b9037ab874f791ba00cddb6fd411ea6
ChoiSuHwan <jerry5230@naver.com>
2024-11-01 00:51:08 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,34 +4,33 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main{
-    static int N;
-    static int arr[],memo[];
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++) {
+        int n = Integer.parseInt(br.readLine());
+        int[] memo = new int[n];
+        int[] arr = new int[n];
+        StringTokenizer st  = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        memo = new int[N];
-        int ans=1;
-        memo[0]=1;
-        for(int i=1;i<N;i++){
-            int maxL=0;
-            for(int j=0;j<i;j++){
-                int tmp=1;
-                if(arr[j]<arr[i]){
-                    tmp=memo[j]+1;
-                }
-                if(maxL<tmp){
-                    maxL=tmp;
-                }
+//        memo[0]=1;
 
+//        if(n>1){
+            for(int i=0;i<n;i++){
+                int tmp=1;
+                for(int j=0;j<i;j++){
+                    if(arr[j]<arr[i]){
+                        tmp =Math.max(tmp,memo[j]+1);
+                    }
+                }
+                memo[i]=tmp;
             }
-            memo[i]=maxL;
-            ans = Math.max(ans,maxL);
+//        }
+        int ans =0;
+        for(int i=0;i<n;i++){
+            ans = Math.max(ans,memo[i]);
         }
         System.out.println(ans);
+
     }
 }
