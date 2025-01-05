f785c0d0a4168ab1a4ca6a3fb8d1df51ecd46eb1
ChoiSuHwan <jerry5230@naver.com>
2025-01-05 21:41:10 +0900

    [Gold II] Title: 확장 게임, Time: 584 ms, Memory: 59456 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -3,116 +3,114 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	
-	static int r,c,p;
 
-	static int dx[] = { 1, -1, 0, 0 };
-	static int dy[] = { 0, 0, 1, -1 };
+    static int r,c,p;
 
-	static int player[];
-	static int[][] map;
+    static int dx[] = { 1, -1, 0, 0 };
+    static int dy[] = { 0, 0, 1, -1 };
 
-	static Queue<Node> q[];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+    static int player[];
+    static int[][] map;
 
-		 st =new StringTokenizer(br.readLine());
-		 r = Integer.parseInt(st.nextToken());
-		 c = Integer.parseInt(st.nextToken());
-		 p = Integer.parseInt(st.nextToken());
-		 
-		 player = new int[p+1];
-		 map = new int[r][c];
-		 q = new LinkedList[p+1];
-		 for(int i=1; i<=p; i++)q[i] = new LinkedList<>();
-		 
-		 st =new StringTokenizer(br.readLine());
-		 for(int i=1; i<=p; i++)player[i] = Integer.parseInt(st.nextToken());
-		 
-		 for(int i=0; i<r; i++) {
-			 String a =br.readLine();
-			 for(int j=0; j<a.length(); j++) {
-				 char tmp = a.charAt(j);
-				 if(tmp>='1' && tmp<='9') {
-				 map[i][j] = tmp-'0';
-				 q[map[i][j]].add(new Node(i,j));
-				 }
-				 else if(tmp =='#')map[i][j]=-1;
-				 if(map[i][j]=='.') {
-					 map[i][j]=0;
-				 }
-			 }
-		 }
-		 
+    static Queue<Node> q[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		 	
-		 boolean flag=false;
-		 while(!flag){
+        st =new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(st.nextToken());
 
-			 int cnt=0;
-			 for(int i=1; i<=p; i++) {
-				if(q[i].size() ==0)cnt++;
-			 }
-			 if(cnt==p) {
-				 flag=true;
-				 break;
-			 }
-			 
-			 for(int i=1; i<=p; i++) {
-				 for(int play=0; play<player[i]; play++ ) {
-				 	int qsize = q[i].size();
-				 if(qsize==0)break;
-				 
-				 while(qsize-->0) {
-					 Node cur = q[i].poll();
-					 
-					 for(int dir=0; dir<4; dir++) {
-						 int nx = cur.x + dx[dir];
-						 int ny = cur.y + dy[dir];
-						 if(nx>=0 && ny>=0 && nx<r && ny<c && map[nx][ny]==0) {
-							 map[nx][ny]=i;
-							 q[i].add(new Node(nx,ny));
-						 }
-						 
-					 }
-				 }
-				}
-				 	
-			 }
-			 
-		 }
-		 
-		 int rst[] = new int[p+1];
-		 for(int i=0; i<r; i++) {
-			 for(int j=0; j<c; j++) {
-				 if(map[i][j]>=1 && map[i][j]<=9)
-				 rst[map[i][j]]++;
-			 }
-		 }
-		 
-		 for(int i=1; i<=p; i++) {
-			 System.out.print(rst[i]+" ");
-		 }
+        player = new int[p+1];
+        map = new int[r][c];
+        q = new ArrayDeque[p+1];
+        for(int i=1; i<=p; i++)q[i] = new ArrayDeque<>();
 
-	}
+        st =new StringTokenizer(br.readLine());
+        for(int i=1; i<=p; i++)player[i] = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<r; i++) {
+            String a =br.readLine();
+            for(int j=0; j<a.length(); j++) {
+                char tmp = a.charAt(j);
+                if(tmp>='1' && tmp<='9') {
+                    map[i][j] = tmp-'0';
+                    q[map[i][j]].add(new Node(i,j));
+                }
+                else if(tmp =='#')map[i][j]=-1;
+                if(map[i][j]=='.') {
+                    map[i][j]=0;
+                }
+            }
+        }
+
+
+
+        boolean flag=false;
+        while(!flag){
+
+            int cnt=0;
+            for(int i=1; i<=p; i++) {
+                if(q[i].size() ==0)cnt++;
+            }
+            if(cnt==p) {
+                flag=true;
+                break;
+            }
+
+            for(int i=1; i<=p; i++) {
+                for(int play=0; play<player[i]; play++ ) {
+                    int qsize = q[i].size();
+                    if(qsize==0)break;
+
+                    while(qsize-->0) {
+                        Node cur = q[i].poll();
+
+                        for(int dir=0; dir<4; dir++) {
+                            int nx = cur.x + dx[dir];
+                            int ny = cur.y + dy[dir];
+                            if(nx>=0 && ny>=0 && nx<r && ny<c && map[nx][ny]==0) {
+                                map[nx][ny]=i;
+                                q[i].add(new Node(nx,ny));
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+
+        int rst[] = new int[p+1];
+        for(int i=0; i<r; i++) {
+            for(int j=0; j<c; j++) {
+                if(map[i][j]>=1 && map[i][j]<=9)
+                    rst[map[i][j]]++;
+            }
+        }
+
+        StringBuilder sb =new StringBuilder();
+        for(int i=1; i<=p; i++) {
+            sb.append(rst[i]).append(" ");
+        }
+        System.out.println(sb);
+
+    }
 
 }
 class Node{
-	int x,y;
+    int x,y;
 
-	public Node(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-	
+    public Node(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+    }
+
 }
