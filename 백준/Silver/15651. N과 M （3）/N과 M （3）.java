b3e94b19fc86f6a2435c6c1a57233261f76963e0
ChoiSuhwan <jerry5230@naver.com>
2025-02-06 17:23:58 +0900

    [Silver III] Title: N과 M (3), Time: 404 ms, Memory: 68644 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,50 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-//순열
-public class Main {
-	
-	static int N;
-	static int M;
-	static int arr[];
-	static int selected[];
-	static boolean visited[];
-	static StringBuilder sb;
-	
-	private static void simulation(int cnt) {
-		if(cnt==M) {
-			for(int x: selected) {
-				sb.append(x+" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=0;i<N;i++) {
-			selected[cnt]=arr[i];
-			visited[i]=true;
-			simulation(cnt+1);
-			visited[i]=false;
-		}
-	}
-	
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Integer.parseInt(st.nextToken());
-    	M = Integer.parseInt(st.nextToken());
-    	sb = new StringBuilder();
-    	arr = new int[N];
-    	selected = new int[M];
-    	visited = new boolean[N];
-    	
-    	for(int i=0;i<N;i++) {
-    		arr[i]=i+1;
-    	}
-    	
-    	simulation(0);
-    	System.out.println(sb);
+public class Main{
+    // 조합
+    static int N,M;
+    static int[] picked;
+    static boolean[] isPicked;
+    static StringBuilder sb = new StringBuilder();
+    private static void pick(int cnt){
+        if(cnt==M){
+            for(int p : picked){
+                sb.append(p).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+
+        for(int i=1;i<=N;i++){
+//            if(isPicked[i]) continue;
+            picked[cnt]=i;
+            isPicked[i]=true;
+            pick(cnt+1);
+            isPicked[i]=false;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isPicked = new boolean[N+1];
+        picked = new int[M];
+        pick(0);
+        System.out.println(sb);
     }
 }
