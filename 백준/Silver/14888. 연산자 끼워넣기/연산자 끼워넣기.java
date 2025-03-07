109f0e15390be1d514dfb3b39cab3a3fc47ae798
ChoiSuhwan <jerry5230@naver.com>
2025-03-07 19:42:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 124 ms, Memory: 15924 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int maxAns=Integer.MIN_VALUE;
+    static int minAns=Integer.MAX_VALUE;
+    static int[] num;
+    static int[] operator;
+    static int[] picked;
+
+    private static int calculate(){
+        int target = num[0];
+
+        for(int i=1;i<N;i++) {
+            switch (picked[i-1]) {
+                case 1: {
+                    target += num[i];
+                    break;
+                }
+                case 2: {
+                    target -= num[i];
+                    break;
+                }
+                case 3: {
+                    target *= num[i];
+                    break;
+                }
+                default: {
+                    if (target < 0) {
+                        target *= -1;
+                        target /= num[i];
+                        target *= -1;
+                        break;
+                    } else {
+                        target /= num[i];
+                        break;
+                    }
+                }
+            }
+        }
+        return target;
+    }
+    private static void simulation(int cnt){
+        if(cnt==(N-1)){
+
+            int result = calculate();
+            maxAns = Math.max(maxAns,result);
+            minAns = Math.min(minAns,result);
+            return;
+        }
+
+        for(int i=1;i<=4;i++){
+            if(operator[i]==0) continue;
+            operator[i]--;
+            picked[cnt]=i;
+            simulation(cnt+1);
+            operator[i]++;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        num = new int[N];
+        picked = new int[N-1];
+        operator = new int[5];
+
+
+        for(int i=0;i<N;i++){
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=4;i++){
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        simulation(0);
+        System.out.println(maxAns);
+        System.out.println(minAns);
+    }
+}
