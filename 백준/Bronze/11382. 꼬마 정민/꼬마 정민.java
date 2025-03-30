bcc4d602ba7b79b110fc87fc9b6da9aced9530e7
ChoiSuhwan <jerry5230@naver.com>
2025-03-30 23:05:21 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        long sum  =a+b+c;
+        System.out.println(sum);
+
+    }
+}
