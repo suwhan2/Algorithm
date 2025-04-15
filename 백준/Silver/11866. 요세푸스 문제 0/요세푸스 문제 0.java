01cded3e244840334b6f64698b65d646308e7672
ChoiSuhwan <jerry5230@naver.com>
2025-04-15 11:40:27 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 136 ms, Memory: 16388 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        StringBuilder sb= new StringBuilder();
+
+        sb.append("<");
+
+        Queue<Integer> q= new ArrayDeque<>();
+        for(int i=1;i<=N;i++){
+            q.offer(i);
+        }
+
+        while(q.size()>1){
+            for(int i=0;i<K-1;i++){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+
+        sb.append(q.poll()).append(">");
+        System.out.println(sb);
+    }
+}
