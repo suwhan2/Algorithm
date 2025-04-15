3f16ccf6874379b69d8b74e9e249f825c572a6a8
ChoiSuhwan <jerry5230@naver.com>
2025-04-15 11:40:59 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 132 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -8,18 +8,13 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
         StringBuilder sb= new StringBuilder();
-
         sb.append("<");
 
         Queue<Integer> q= new ArrayDeque<>();
-        for(int i=1;i<=N;i++){
-            q.offer(i);
-        }
+        for(int i=1;i<=N;i++){ q.offer(i); }
 
         while(q.size()>1){
-            for(int i=0;i<K-1;i++){
-                q.offer(q.poll());
-            }
+            for(int i=0;i<K-1;i++){ q.offer(q.poll()); }
             sb.append(q.poll()).append(", ");
         }
 
