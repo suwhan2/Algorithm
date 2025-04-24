fe7f2fdc48ba5125e822058bdfcbf474667934d9
ChoiSuhwan <jerry5230@naver.com>
2025-04-24 17:21:43 +0900

    [Gold IV] Title: 거짓말, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,70 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N,M;
-	static int person[];
-	static boolean visited[];
-	static List<Integer> bfsList;
-	static Queue<Integer> q = new ArrayDeque<>();
-	public static void main(String[] args)throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		person= new  int[N+1];
-		visited = new boolean[N+1];
-		bfsList = new ArrayList<>();
-		List<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-		for(int i=0;i<N+1;i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		st= new StringTokenizer(br.readLine());
-		int n=Integer.parseInt(st.nextToken());
-		for(int i=0;i<n;i++) {
-			bfsList.add(Integer.parseInt(st.nextToken()));
-		}
-		
-		for(int i=0;i<M;i++) {
-			st=new StringTokenizer(br.readLine());
-			int k = Integer.parseInt(st.nextToken());
-			int start =Integer.parseInt(st.nextToken());
-			person[start]++;
-			
-			for(int j=0;j<k-1;j++) {
-				int p = Integer.parseInt(st.nextToken());
-				graph.get(p).add(start);
-				graph.get(start).add(p);
-				start = p;
-			}
-		}
-		for(int x : bfsList) {
-			if(!visited[x]) {
-				q.offer(x);
-				person[x]=0;
-				while(!q.isEmpty()) {
-					int tmp = q.poll();
-					for(int i=0;i<graph.get(tmp).size();i++) {
-						int target = graph.get(tmp).get(i);
-						if(visited[target]) continue;
-						q.offer(target);
-						visited[target]=true;
-						person[target]=0;
-					}
-				}
-			}
-		}
-		int ans=0;
-		for(int i=0;i<N+1;i++) {
-			ans+=person[i];
-		}
-		System.out.println(ans);
-	}
+    static int N,M;
+    static List<Integer> result;
+    static int[] parents;
+    private static int find(int n){
+        if(parents[n]==n) return n;
+
+        return find(parents[n]);
+    }
+    private static boolean union(int a,int b){
+        int pa = find(a);
+        int pb = find(b);
+
+        //부모가 같으면 이미 같은 그룹 합칠 이유가 없다
+        if(pa==pb) return false;
+
+        if(pa<pb) parents[pb]=pa;
+        else parents[pa]=pb;
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result=new ArrayList<>();
+        parents=new int[N+1];
+        for(int i=1;i<=N;i++){
+            parents[i]=i;
+        }
+
+        st= new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++){
+            int tmp = Integer.parseInt(st.nextToken());
+            parents[tmp]=0;
+        }
+
+
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int c = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+
+            for(int j=0;j<c-1;j++){
+                int end = Integer.parseInt(st.nextToken());
+                union(start,end);
+            }
+
+            result.add(find(start));
+        }
+
+        
+
+        int cnt=0;
+        for(Integer i : result){
+            if(find(i)!=0) cnt++;
+        }
+        System.out.println(cnt);
+
+    }
 }
