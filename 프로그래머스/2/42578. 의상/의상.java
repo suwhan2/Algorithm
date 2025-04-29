b75d6d3c228ce22311a617200b6243dd5e651f89
ChoiSuhwan <jerry5230@naver.com>
2025-04-29 18:08:18 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String[] s: clothes){
+            map.put(s[1],map.getOrDefault(s[1],0)+1);
+        
+        }
+        
+        int answer = 1;
+        
+        for(String key : map.keySet()){
+            answer*=(map.get(key)+1);
+        }
+        return answer-1;
+    }
+}
